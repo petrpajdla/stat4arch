71072d5f278fd744cfa5c6190270ae0cc7f22ebf
petrpajdla <petr.pajdla@protonmail.com>
2024-06-07 20:18:30 +0200

    add project templates

---
+++ project/template.r
@@ -0,0 +1,64 @@
+# Script title
+# Author
+# Date
+
+
+# Abstract ----------------------------------------------------------------
+
+# Short abstract describing what is the dataset about and what you are 
+# focusing on in the analysis (one paragraph of text at most).
+
+
+# Keywords
+
+# Include several useful keywords.
+
+
+# Introduction ------------------------------------------------------------
+
+# Mention what is the goal of your analysis, 
+# why did you choose this dataset etc. in a short introduction.
+
+# Load your packages here
+# library(here)
+# library(dplyr)
+# library(ggplot2)
+
+
+# Data --------------------------------------------------------------------
+
+# Briefly introduce your dataset.
+# Where does it come from? 
+# What does it contain? 
+# What are the variables and observations? 
+# How did you get the dataset? 
+# Where is it originally published (include citations, links etc.) 
+
+# my_dataset <- read.csv(here::here("path/to/your/dataset.csv"))
+
+# Try to answer following questions using code:
+# How many variables are there?
+# How many rows does the dataset have?
+# What are the names of the variables?
+# What data types are there in the data set?
+
+# ncol(my_dataset)
+
+# Analysis ----------------------------------------------------------------
+
+# Focus on the analysis of your data set. 
+# Are there any quantitative and qualitative variables? 
+# Show answers to questions like these on graphs and 
+# using various numerical summaries or tests:
+# How are the variables distributed?
+# What are their central points and how are they spread? What does it mean?
+# Is any of the quantitative variables distributed normally?
+# What are the relationships between the variables?
+# If your points, sites etc. have coordinates, show them on a simple map.
+
+
+# Summary -----------------------------------------------------------------
+
+# Briefly sum up and interpret what you managed to achieve in your project. 
+# Did you learn anything new? 
+# Did anything surprise you?
